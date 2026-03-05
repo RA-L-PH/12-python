b7905c9ed474171ad2763cdfc8c14dda84af60ae
RA-L-PH <ralphaacarvalho@gmail.com>
2026-03-05 09:42:09 +0530

    deleted certain files

--- 07_palindrome_checker.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(text):
-    cleaned = "".join(ch.lower() for ch in text if ch.isalnum())
-    return cleaned == cleaned[::-1]
-
-word = input("Enter a word or phrase: ")
-if is_palindrome(word):
-    print(f'"{word}" is a palindrome!')
-else:
-    print(f'"{word}" is not a palindrome.')
